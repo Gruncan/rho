622f3ee088311917f661dcebb6db23fe51e464e8
Gruncan <dj226@outlook.com>
2022-01-27 16:06:32 +0000

    Improved GameObject

--- src/main/java/net/rho/core/GameObject.java
+++ src/main/java/net/rho/core/GameObject.java
@@ -1,54 +1,58 @@
 package net.rho.core;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class GameObject {
 
     private final String name;
-    private final List<Component> components;
+    private final Map<Class<? extends Component>, Component> components;
 
     public GameObject(String name){
         this.name = name;
-        this.components = new ArrayList<>();
+        this.components = new HashMap<>();
     }
 
     public <T extends Component> T getComponent(Class<T> componentClass){
-        for (Component c : components){
-            if (componentClass.isAssignableFrom(c.getClass())){
-                try {
-                    return componentClass.cast(c);
-                }catch (ClassCastException e){
-                    e.printStackTrace();
-                    assert false : "Error : Casting component.";
-                }
-            }
-        }
-        return null;
+        return componentClass.cast(this.components.get(componentClass));
+//        for (Component c : components){
+//            if (componentClass.isAssignableFrom(c.getClass())){
+//                try {
+//                    return componentClass.cast(c);
+//                }catch (ClassCastException e){
+//                    e.printStackTrace();
+//                    assert false : "Error : Casting component.";
+//                }
+//            }
+//        }
+//        return null;
     }
 
     public <T extends Component> void removeComponent(Class<T> componentClass){
-        for (int i=0; i< components.size(); i++) {
-            if (componentClass.isAssignableFrom(components.get(i).getClass())){
-                components.remove(i);
-                return;
-            }
-        }
+        this.components.remove(componentClass);
+//        for (int i=0; i< components.size(); i++) {
+//            if (componentClass.isAssignableFrom(components.get(i).getClass())){
+//                components.remove(i);
+//                return;
+//            }
+//        }
     }
 
 
     public void addComponent(Component c){
-        this.components.add(c);
+        this.components.put(c.getClass(), c);
         c.gameObject = this;
     }
 
     public void update(float dt){
-        for (Component component : components) {
+        for (Component component : this.components.values()) {
             component.update(dt);
         }
     }
     public void start(){
-        for (Component component : components) {
+        for (Component component : this.components.values()) {
             component.start();
         }
     }
