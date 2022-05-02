ab6528bdb045f9098421d043feda87ab155058d6
Duncan Jones <2561101J@student.gla.ac.uk>
2022-05-02 21:54:41 +0100

    Removed snake game from main code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,9 @@
-import net.rho.core.Window;
-import testgame.SnakeScene;
+
 
 public class Main {
 
 
     public static void main(String[] args) {
-        Window window = Window.getInstance();
-        window.setWindow("Snake Game", 1920, 1080);
 
-
-        window.addScene(0, new SnakeScene());
-
-        window.run();
     }
 }
